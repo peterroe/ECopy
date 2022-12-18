4fe8b0c85e4b40f1cb8f2556a3152c8c4b2b28d6
peterroe <peterroe@163.com>
2022-12-18 23:05:49 +0800

    fix: home dir

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,16 @@
+use eframe::egui::TextBuffer;
 #[macro_use]
 use lazy_static::lazy_static;
 
 use arboard::Clipboard;
+use home::home_dir;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::{self, json, Value};
-use std::path::Path;
+use std::env;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::time::{SystemTime, UNIX_EPOCH};
+// use users::os::unix::UserExt;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EcopyJson {
     pub name: String,
@@ -29,20 +33,20 @@ impl EcopyJson {
         json!({
             "name": "Ecopy2",
             "data": [{
-                "content": "第一条复制的好好的地道",
-                "time": ms + 10020
-            },{
-                "content": "g::builder().add_source(config",
-                "time": ms + 2300
-            },{
-                "content": "ttings_path = std::path::",
-                "time": ms + 44300
-            },{
-                "content": "board on  main [⇡!] is 📦 1.0.0 ",
+                "content": "复制的字符串内容",
                 "time": ms + 1000
             },{
-                "content": " 📦 1.0.0 via 🦀 1.65",
-                "time": ms + 10120
+                "content": "都会被检测到",
+                "time": ms + 800
+            },{
+                "content": "======",
+                "time": ms + 650
+            },{
+                "content": "点击当前行",
+                "time": ms + 400
+            },{
+                "content": "可以复制当前行的内容",
+                "time": ms + 300
             }],
         })
     }
@@ -55,23 +59,27 @@ impl EcopyJson {
             })
         })
         .unwrap();
-        std::fs::write(PATH, serialized).unwrap();
+        std::fs::write(get_config_dir(), serialized).unwrap();
     }
 }
 
-static PATH: &str = "/Users/lsh/Desktop/t/packages/rust100/clipboard/src/fuck.json";
+// static mut get_config_dir().to_str().unwrap(): &str = "";
+fn get_config_dir() -> PathBuf {
+    home_dir().unwrap().join(Path::new(".ecopy.json"))
+}
 
 pub fn init_e_copy_json() {
-    let exist_json = Path::new(PATH).exists();
+    print!("get_config_dir(), {:?}", get_config_dir());
+    let exist_json = Path::new::<PathBuf>(&get_config_dir()).exists();
     if !exist_json {
         let d = EcopyJson::create_default();
         let serialized = serde_json::to_string(&d).unwrap();
-        std::fs::write(PATH, serialized).unwrap();
+        std::fs::write(get_config_dir(), serialized).unwrap();
     }
 }
 
 pub fn get_e_copy_json() -> EcopyJson {
-    let json_str = std::fs::read_to_string(PATH).unwrap();
+    let json_str = std::fs::read_to_string(get_config_dir()).unwrap();
     let mut c: EcopyJson = serde_json::from_str(&json_str).unwrap();
     println!("hello: {:?}", c.data);
     c.data.sort_by(|a, b| b.time.cmp(&a.time));
@@ -80,7 +88,7 @@ pub fn get_e_copy_json() -> EcopyJson {
 
 pub fn set_e_copy_json(data: EcopyJson) {
     let o = serde_json::to_string::<EcopyJson>(&data).unwrap();
-    std::fs::write(PATH, o).unwrap();
+    std::fs::write(get_config_dir(), o).unwrap();
 }
 
 lazy_static! {
